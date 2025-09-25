b36f254d96fba1c05b7014be2a7c66bb4b8b63a0
MikhailMilyaev <milyaevmisha@gmail.com>
2025-09-25 22:15:16 +0300

    add docker compose plus sync script

---
+++ server/scripts/sync.js
@@ -0,0 +1,4 @@
+require('dotenv').config({path:'.env'})
+const sequelize = require('../db')
+require('../models/models')
+sequelize.sync({force:true}).then(()=>{console.log('synced');process.exit(0)}).catch(e=>{console.error(e);process.exit(1)})
